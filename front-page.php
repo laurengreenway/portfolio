01e8371369ef83191db0eb7a2d2f76812480dbf8
Lauren Greenway <lagreenway@gmail.com>
2016-11-09 14:00:48 -0500

    added portfolio layout, not positioned yet

--- front-page.php
+++ front-page.php
@@ -32,54 +32,82 @@
           </div>
 
         </section> <!-- end about -->
+      </div>
         <section class="portfolio">
           <h2>PORTFOLIO AND SHIT</h2>
-        </section>
+            <?php  
+              $portfolioArgs = array(
+                'post_type' => 'portfolio_items'
+              );
+              $portfolioLoop = new WP_Query($portfolioArgs);
+
+              if($portfolioLoop->have_posts()) while($portfolioLoop->have_posts()) : $portfolioLoop->the_post();
+            ?>
+              <div class="portfolioItem grid">
+                <div class="sideways">
+                  <h3><?php the_title(); ?></h3>
+                </div>
+                <div class="portfolioDescription">
+                  <p><?php the_content(); ?></p>
+                </div>
+                <div class="portfolioImage">
+                  <img src="<?php the_post_thumbnail_url('large') ?>" alt="">
+                </div>
+              </div>
+                
+          <?php endwhile; ?>
+          <?php wp_reset_postdata(); ?>
+        </section> <!-- end portfolio -->
         <section class="skills container grid">
-          <div class="icon grid-cell">
+          <div class="icon grid-cell animated wiggle">
             <span class="devicons devicons-html5"></span>
             <p>HTML 5</p>
           </div>
-          <div class="icon grid-cell">
+          <div class="icon grid-cell animated wiggle">
             <span class="devicons devicons-css3"></span>
             <p>CSS 3</p>
           </div>
-          <div class="icon grid-cell">
+          <div class="icon grid-cell animated wiggle">
             <span class="devicons devicons-sass"></span>
             <p>Sass</p>
           </div>
-          <div class="icon grid-cell">
+          <div class="icon grid-cell animated wiggle">
             <span class="devicons devicons-javascript"></span>
             <p>JavaScript</p>
           </div>
-          <div class="icon grid-cell">
+          <div class="icon grid-cell animated wiggle">
             <span class="devicons devicons-jquery"></span>
             <p>jQuery</p>
           </div>
-          <div class="icon grid-cell">
+          <div class="icon grid-cell animated wiggle">
             <span class="devicons devicons-gulp"></span>
             <p>Gulp</p>
           </div>
-          <div class="icon grid-cell">
+          <div class="icon grid-cell animated wiggle">
             <span class="devicons devicons-terminal"></span>
             <p>Command Line</p>
           </div>
-          <div class="icon grid-cell">
+          <div class="icon grid-cell animated wiggle">
             <span class="devicons devicons-wordpress"></span>
             <p>Wordpress</p>
           </div>
-          <div class="icon grid-cell">
+          <div class="icon grid-cell animated wiggle">
             <span class="devicons devicons-git"></span>
             <p>Git</p>
           </div>
-          <div class="icon grid-cell">
+          <div class="icon grid-cell animated wiggle">
             <span class="devicons devicons-github_badge"></span>
             <p>GitHub</p>
           </div>
-          <div class="icon grid-cell">
+          <div class="icon grid-cell animated wiggle">
             <span class="devicons devicons-sublime"></span>
             <p>Sublime Text</p>
           </div>
+          <div class="icon grid-cell animated wiggle">
+            <span class="devicons devicons-trello"></span>
+            <p>Trello</p>
+          </div>
+          
         </section>
         <section class="contact">
           <div class="content grid">
