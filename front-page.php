fcdff131f673b206b6ec5f4c61c2edfe5d1075f3
Lauren Greenway <lagreenway@gmail.com>
2016-11-09 10:44:11 -0500

    Wednesday Morning

--- front-page.php
+++ front-page.php
@@ -2,18 +2,92 @@
 
 <div class="main">
   <div class="container">
-
-    <div class="content">
       <?php // Start the loop ?>
       <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+        <section class="about" id="about">
 
-        <h2><?php the_title(); ?></h2>
-        <?php the_content(); ?>
+          <div class="aboutFancy grid">
 
+            <div class="seafoamSquare grid-cell">
+            </div>
+
+            <div class="avatar grid-cell">
+              <img src="<?php echo the_field('about_image') ?>" alt="Lauren Greenway Profile Image">
+            </div>
+
+          </div>
+          <figcaption class="content grid-cell">
+              <h4><?php the_field('about_me_caption'); ?></h4>
+          </figcaption>
+
+          <div class="content">
+            <h2> <?php the_field('about_me_title'); ?> </h2>
+            <?php the_field('about_me'); ?>
+          </div>
+          <div class="sideColumn">
+            <h3>D</h3>
+            <h3>O</h3>
+            <h3>P</h3>
+            <h3>E</h3>
+          </div>
+
+        </section> <!-- end about -->
+        <section class="portfolio">
+          <h2>PORTFOLIO AND SHIT</h2>
+        </section>
+        <section class="skills container grid">
+          <div class="icon grid-cell">
+            <span class="devicons devicons-html5"></span>
+            <p>HTML 5</p>
+          </div>
+          <div class="icon grid-cell">
+            <span class="devicons devicons-css3"></span>
+            <p>CSS 3</p>
+          </div>
+          <div class="icon grid-cell">
+            <span class="devicons devicons-sass"></span>
+            <p>Sass</p>
+          </div>
+          <div class="icon grid-cell">
+            <span class="devicons devicons-javascript"></span>
+            <p>JavaScript</p>
+          </div>
+          <div class="icon grid-cell">
+            <span class="devicons devicons-jquery"></span>
+            <p>jQuery</p>
+          </div>
+          <div class="icon grid-cell">
+            <span class="devicons devicons-gulp"></span>
+            <p>Gulp</p>
+          </div>
+          <div class="icon grid-cell">
+            <span class="devicons devicons-terminal"></span>
+            <p>Command Line</p>
+          </div>
+          <div class="icon grid-cell">
+            <span class="devicons devicons-wordpress"></span>
+            <p>Wordpress</p>
+          </div>
+          <div class="icon grid-cell">
+            <span class="devicons devicons-git"></span>
+            <p>Git</p>
+          </div>
+          <div class="icon grid-cell">
+            <span class="devicons devicons-github_badge"></span>
+            <p>GitHub</p>
+          </div>
+          <div class="icon grid-cell">
+            <span class="devicons devicons-sublime"></span>
+            <p>Sublime Text</p>
+          </div>
+        </section>
+        <section class="contact">
+          <div class="content grid">
+            <?php the_content(); ?>
+          </div>
+        </section>
       <?php endwhile; // end the loop?>
     </div> <!-- /,content -->
-
-    <?php get_sidebar(); ?>
 
   </div> <!-- /.container -->
 </div> <!-- /.main -->
--- header.php
+++ header.php
@@ -5,6 +5,9 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?php  wp_title('|', true, 'right'); ?></title>
+  <link href="https://fonts.googleapis.com/css?family=Inconsolata|Montserrat|Roboto:300,400" rel="stylesheet">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
+  <link href='http://cdn.jsdelivr.net/devicons/1.8.0/css/devicons.min.css' rel='stylesheet'>
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
   <!-- stylesheets should be enqueued in functions.php -->
   <?php wp_head(); ?>
@@ -13,18 +16,30 @@
 
 <body <?php body_class(); ?>>
 
-<header>
-  <div class="container">
-    <h1>
-      <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
-        <?php bloginfo( 'name' ); ?>
-      </a>
-    </h1>
-
-    <?php wp_nav_menu( array(
+<?php $url = wp_get_attachment_url( get_post_thumbnail_id() ); ?>
+<nav class="grid">
+      <?php wp_nav_menu( array(
       'container' => false,
       'theme_location' => 'primary'
     )); ?>
+  <h2>Lauren Codes</h2>
+      <?php wp_nav_menu( array(
+      'container' => false,
+      'theme_location' => 'social'
+    )); ?>
+</nav>
+<header style="background-image: url(<?php echo $url ?>);" >
+  <div class="container">
+
+    <div class="triangleBox">
+    </div>
+     <h1>
+      <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
+        LAUREN CODES
+      </a>
+    </h1>
+
+
   </div> <!-- /.container -->
 </header><!--/.header-->
 
